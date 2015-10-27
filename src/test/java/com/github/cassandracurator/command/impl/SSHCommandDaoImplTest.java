53068b0b3c8c3d6f7a14b5f00061d34ddb24a609
Jeffrey DeYoung <Jeffrey.J.DeYoung@gmail.com>
2015-10-26 19:47:26 -0600

    placeholder for ssh tests

---
+++ src/test/java/com/github/cassandracurator/command/impl/SSHCommandDaoImplTest.java
@@ -0,0 +1,113 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.github.cassandracurator.command.impl;
+
+import java.io.File;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import static org.junit.Assert.*;
+import org.junit.Ignore;
+
+/**
+ *
+ * @author jeffrey
+ */
+public class SSHCommandDaoImplTest {
+    
+    public SSHCommandDaoImplTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of connect method, of class SSHCommandDaoImpl.
+     */
+    @org.junit.Test
+    @Ignore
+    public void testConnect() throws Exception {
+        System.out.println("connect");
+        SSHCommandDaoImpl instance = null;
+        instance.connect();
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of logOff method, of class SSHCommandDaoImpl.
+     */
+    @org.junit.Test
+    @Ignore
+    public void testLogOff() {
+        System.out.println("logOff");
+        SSHCommandDaoImpl instance = null;
+        instance.logOff();
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of pullFile method, of class SSHCommandDaoImpl.
+     */
+    @org.junit.Test
+    @Ignore
+    public void testPullFile() throws Exception {
+        System.out.println("pullFile");
+        String remotePathToPull = "";
+        File localFile = null;
+        SSHCommandDaoImpl instance = null;
+        instance.pullFile(remotePathToPull, localFile);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of pushFile method, of class SSHCommandDaoImpl.
+     */
+    @org.junit.Test
+    @Ignore
+    public void testPushFile() throws Exception {
+        System.out.println("pushFile");
+        File localFile = null;
+        String remotePath = "";
+        SSHCommandDaoImpl instance = null;
+        instance.pushFile(localFile, remotePath);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of sendCommand method, of class SSHCommandDaoImpl.
+     */
+    @org.junit.Test
+    @Ignore
+    public void testSendCommand() throws Exception {
+        System.out.println("sendCommand");
+        String commandToSend = "";
+        SSHCommandDaoImpl instance = null;
+        String expResult = "";
+        String result = instance.sendCommand(commandToSend);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
