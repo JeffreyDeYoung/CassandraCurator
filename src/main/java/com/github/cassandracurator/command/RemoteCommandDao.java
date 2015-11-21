b215b14585b8a1d2716a5d99e4b97b1273e80282
Jeffrey DeYoung <Jeffrey.J.DeYoung@gmail.com>
2015-11-20 18:33:17 -0700

    javadoc improvments

--- src/main/java/com/github/cassandracurator/command/RemoteCommandDao.java
+++ src/main/java/com/github/cassandracurator/command/RemoteCommandDao.java
@@ -22,24 +22,28 @@ import java.io.IOException;
 
 /**
  * Dao for interacting with remote systems using standard terminal commands.
+ *
  * @author jeffrey
  */
 public interface RemoteCommandDao {
-    
+
     /**
      * Establishes a connection to a remote system.
+     *
      * @throws CannotConnectException if there is an problem connecting.
      */
     public void connect() throws CannotConnectException;
-    
+
     /**
      * Logs off of the remote system.
      */
     public void logOff();
-    
+
     /**
      * Sends a command to a remote system.
-     * @param commandToSend String of a valid terminal command to send to the remote system.
+     *
+     * @param commandToSend String of a valid terminal command to send to the
+     * remote system.
      * @return The response from the remote system as a String.
      * @throws ConnectionException if there is a problem with the connection.
      */
@@ -47,19 +51,22 @@ public interface RemoteCommandDao {
 
     /**
      * Pushes a file from the local machine to the remote machine.
+     *
      * @param localFile Local file to push.
-     * @param remotePath Path on the remote machine to push to.
+     * @param remotePath Directory path on the remote machine to push to.
      * @throws ConnectionException if there is a problem with the connection.
      * @throws IOException if there is a problem reading or sending the file.
      */
     public void pushFile(File localFile, String remotePath) throws ConnectionException, IOException;
-    
-        /**
-     * Pushes a file from the local machine to the remote machine.
+
+    /**
+     * Pulls a file from the remote to the local machine.
+     *
      * @param localFile Local file to write the remote file to.
-     * @param remotePathToPull File path on the remote machine to full from.
+     * @param remoteFileToPull File path on the remote machine to pull the file
+     * from.
      * @throws ConnectionException if there is a problem with the connection.
      * @throws IOException if there is a problem pulling or writing the file.
      */
-    public void pullFile(String remotePathToPull, File localFile) throws ConnectionException, IOException;
+    public void pullFile(String remoteFileToPull, File localFile) throws ConnectionException, IOException;
 }
