0a2b158e333e75ee070957d486095d5e6af7a97e
Jeffrey DeYoung <Jeffrey.J.DeYoung@gmail.com>
2016-01-10 17:18:51 -0700

    cleaned up the startCassandra call

--- src/main/java/com/github/cassandracurator/functions/CassandraCommandFunction.java
+++ src/main/java/com/github/cassandracurator/functions/CassandraCommandFunction.java
@@ -26,12 +26,33 @@ import java.io.IOException;
 public class CassandraCommandFunction
 {
 
-    public static void startCassandra(RemoteCommandDao command) throws ConnectionException, IOException, InterruptedException
+    private static final String EXPECTED_RUNNING_MESSAGE = "* Cassandra is running";
+    /**
+     * Starts an Cassandra service using the specified RemoteCommandDao.
+     *
+     * @param command RemoteCommandDao that is <b>already connected</b> to the
+     * server you wish to start the Cassandra instance on.
+     * @throws ConnectionException If we can't connect or there is an connection
+     * problem to the server.
+     * @throws IOException If there is an IO issue talking to the server.
+     * @return True if cassandra was started; false if it failed to start.
+     */
+    public static boolean startCassandra(RemoteCommandDao command) throws ConnectionException, IOException
     {
-        command.sendCommand("/etc/cassandra/setcassandraip.sh");//set the ips in the cassandra yaml correctly; I can't get the dockerfile to do this automatically
+        command.sendCommand("/etc/cassandra/setcassandraip.sh");//set the ips in the cassandra yaml correctly; I can't get the dockerfile to do this automatically -- this will probably bite us in the future
         command.sendCommand("service cassandra start");//make the call to start cassandra
-        Thread.sleep(15000);//Sleep to let cassandra finish starting up
-        //TODO: cleanup, java doc, finish, etc, not working
-
+        try
+        {
+            Thread.sleep(15000);//Sleep to let cassandra finish starting up
+        } catch (InterruptedException e)
+        {
+            throw new RuntimeException(e);//in my over 13 years working with java, I have never seen an InterruptedException thrown for calling sleep; so I think this is pretty safe.
+        }
+        String statusResponse = command.sendCommand("service cassandra status");
+        if(statusResponse.equals(EXPECTED_RUNNING_MESSAGE)){
+            return true;
+        } else {
+            return false;
+        }
     }
 }
---
+++ src/test/java/com/github/cassandracurator/functions/CassandraCommandFunctionTest.java
@@ -0,0 +1,87 @@
+/*
+ * Copyright 2016 jeffrey.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.github.cassandracurator.functions;
+
+import com.github.cassandracurator.command.RemoteCommandDao;
+import com.github.cassandracurator.command.impl.SSHCommandDaoImpl;
+import com.github.cassandracurator.exceptions.ConnectionException;
+import com.github.cassandracurator.testhelper.DockerHelper;
+import java.io.File;
+import java.io.IOException;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author jeffrey
+ */
+public class CassandraCommandFunctionTest
+{
+
+    private String dockerIp = null;
+    private String dockerId = null;
+    private RemoteCommandDao commandDao = null;
+
+    public CassandraCommandFunctionTest()
+    {
+    }
+
+    @BeforeClass
+    public static void setUpClass()
+    {
+    }
+
+    @AfterClass
+    public static void tearDownClass()
+    {
+    }
+
+    @Before
+    public void setUp() throws ConnectionException, IOException, InterruptedException
+    {
+        dockerId = DockerHelper.spinUpDockerBox("cassandra2.1.0", new File("./src/test/resources/docker/cassandra2.1.0"));
+        dockerIp = DockerHelper.getDockerIp(dockerId);
+        commandDao = new SSHCommandDaoImpl(dockerIp, "root", 22, "./src/test/resources/docker/insecure_key", null);
+        commandDao.connect();
+
+    }
+
+    @After
+    public void tearDown()
+    {
+        if (dockerId != null)
+        {
+            DockerHelper.spinDownDockerBox(dockerId);
+        }
+    }
+
+    /**
+     * Test of startCassandra method, of class CassandraCommandFunction.
+     */
+    @Test
+    public void testStartCassandra() throws Exception
+    {
+        System.out.println("startCassandra");
+        boolean expResult = true;
+        boolean result = CassandraCommandFunction.startCassandra(commandDao);
+        assertEquals(expResult, result);
+    }
+
+}
